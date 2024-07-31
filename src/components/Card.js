c64bbd13ffaeaae9079e0c40a126856f29ef80a4
Floresa Zogaj <floresaa.zogaj@gmail.com>
2024-07-31 20:14:18 +0200

    FirstAsssignment

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,13 @@
+// Card.js
 import React from 'react';
 import './Card.css';
 
 const Card = ({ imageSrc, title, description }) => {
     return (
         <div className="card">
-            <img src={imageSrc} alt={title} className="image" />
+            <div className="image-wrapper">
+                <img src={imageSrc} alt={title} className="image" />
+            </div>
             <div className="text-content">
                 <div className="title">{title}</div>
                 <div className="description">{description}</div>
